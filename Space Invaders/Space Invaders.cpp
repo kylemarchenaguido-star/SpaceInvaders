1f83290040205985358375e04605147b11e91236
kylemarchenaguido-star <kylemarchenaguido@gmail.com>
2026-03-13 00:20:33 -0600

    Add project files.

---
+++ Space Invaders/Space Invaders.cpp
@@ -0,0 +1,13 @@
+﻿// Space Invaders.cpp : Defines the entry point for the application.
+//
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include "Space Invaders.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
