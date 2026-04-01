da4ff987fc32c5b8ace9789a76b06332651b60f6
kylemarchenaguido-star <kylemarchenaguido@gmail.com>
2026-04-01 01:50:53 -0600

    Final realease and working version

--- Space Invaders/Space Invaders.cpp
+++ Space Invaders/Space Invaders.cpp
@@ -1,10 +1,4 @@
-﻿// Space Invaders.cpp : Defines the entry point for the application.
-//
-
-#include "Space Invaders.h"
-
-using namespace std;
-
+﻿#include "Space Invaders.h"
 
 int main()
 {
@@ -20,16 +14,22 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	//This is the openGL window and context we will be rendering to
-    GLFWwindow* window = glfwCreateWindow(buffer_width, buffer_height, "Space Invaders", NULL, NULL);
-    if (!window) { glfwTerminate(); return -1; }
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* window = glfwCreateWindow(2 * buffer_width, 2 * buffer_height, "Space Invaders", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetKeyCallback(window, key_callback);
 
     glfwMakeContextCurrent(window);
 
     GLenum err = glewInit();
-
-    if (err != GLEW_OK) {
-        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    if (err != GLEW_OK)
+    {
+        fprintf(stderr, "Error initializing GLEW.\n");
         glfwTerminate();
         return -1;
     }
@@ -37,7 +37,6 @@ int main()
     int glVersion[2] = { -1, 1 };
     glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
     glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]);
-    printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
 
     gl_debug(__FILE__, __LINE__);
 
@@ -47,43 +46,63 @@ int main()
 
     glfwSwapInterval(1);
 
-    uint32_t clear_color = rgb_to_uint32(0, 128, 0);
-
     glClearColor(1.0, 0.0, 0.0, 1.0);
 
-
-    // Create buffer
+    // Create graphics buffer
     Buffer buffer;
     buffer.width = buffer_width;
     buffer.height = buffer_height;
     buffer.data = new uint32_t[buffer.width * buffer.height];
-    buffer_clear(&buffer, clear_color);
 
-    // VAO
+    buffer_clear(&buffer, 0);
+
+    // Create texture for presenting buffer to OpenGL
+    GLuint buffer_texture;
+    glGenTextures(1, &buffer_texture);
+    glBindTexture(GL_TEXTURE_2D, buffer_texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, buffer.width, buffer.height, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer.data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+
+    // Create vao for generating fullscreen triangle
     GLuint fullscreen_triangle_vao;
     glGenVertexArrays(1, &fullscreen_triangle_vao);
 
-    // Shaders
+
+    // Create shader for displaying buffer
+  
+
     GLuint shader_id = glCreateProgram();
+
     {
+        //Create vertex shader
         GLuint shader_vp = glCreateShader(GL_VERTEX_SHADER);
+
         glShaderSource(shader_vp, 1, &vertex_shader, 0);
         glCompileShader(shader_vp);
         validate_shader(shader_vp, vertex_shader);
         glAttachShader(shader_id, shader_vp);
+
         glDeleteShader(shader_vp);
     }
+
     {
+        //Create fragment shader
         GLuint shader_fp = glCreateShader(GL_FRAGMENT_SHADER);
+
         glShaderSource(shader_fp, 1, &fragment_shader, 0);
         glCompileShader(shader_fp);
         validate_shader(shader_fp, fragment_shader);
         glAttachShader(shader_id, shader_fp);
+
         glDeleteShader(shader_fp);
     }
 
-    //Link the program
     glLinkProgram(shader_id);
+
     if (!validate_program(shader_id)) {
         fprintf(stderr, "Error while validating shader.\n");
         glfwTerminate();
@@ -92,45 +111,66 @@ int main()
         return -1;
     }
 
-	// Set uniform (program must be linked before this step)
     glUseProgram(shader_id);
+
     GLint location = glGetUniformLocation(shader_id, "buffer");
     glUniform1i(location, 0);
 
-    // Texture
-    GLuint buffer_texture;
-    glGenTextures(1, &buffer_texture);
-    glBindTexture(GL_TEXTURE_2D, buffer_texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8,buffer.width, buffer.height, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer.data);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-    //Open
+    //OpenGL setup
     glDisable(GL_DEPTH_TEST);
+    glActiveTexture(GL_TEXTURE0);
+
     glBindVertexArray(fullscreen_triangle_vao);
 
-	// Game objects
-    Sprite alien_sprite;
-    alien_sprite.width = 11;
-    alien_sprite.height = 8;
-    alien_sprite.data = new uint8_t[88]
+    // Prepare game
+    Sprite alien_sprites[6];
+
+    alien_sprites[0].width = 8;
+    alien_sprites[0].height = 8;
+    alien_sprites[0].data = new uint8_t[64]
     {
-        0,0,1,0,0,0,0,0,1,0,0,
-        0,0,0,1,0,0,0,1,0,0,0,
-        0,0,1,1,1,1,1,1,1,0,0,
-        0,1,1,0,1,1,1,0,1,1,0,
-        1,1,1,1,1,1,1,1,1,1,1,
-        1,0,1,1,1,1,1,1,1,0,1,
-        1,0,1,0,0,0,0,0,1,0,1,
-        0,0,0,1,1,0,1,1,0,0,0
+        0,0,0,1,1,0,0,0, // ...@@...
+        0,0,1,1,1,1,0,0, // ..@@@@..
+        0,1,1,1,1,1,1,0, // .@@@@@@.
+        1,1,0,1,1,0,1,1, // @@.@@.@@
+        1,1,1,1,1,1,1,1, // @@@@@@@@
+        0,1,0,1,1,0,1,0, // .@.@@.@.
+        1,0,0,0,0,0,0,1, // @......@
+        0,1,0,0,0,0,1,0  // .@....@.
     };
 
-    Sprite alien_sprite1;
-    alien_sprite1.width = 11;
-    alien_sprite1.height = 8;
-    alien_sprite1.data = new uint8_t[88]
+    alien_sprites[1].width = 8;
+    alien_sprites[1].height = 8;
+    alien_sprites[1].data = new uint8_t[64]
+    {
+        0,0,0,1,1,0,0,0, // ...@@...
+        0,0,1,1,1,1,0,0, // ..@@@@..
+        0,1,1,1,1,1,1,0, // .@@@@@@.
+        1,1,0,1,1,0,1,1, // @@.@@.@@
+        1,1,1,1,1,1,1,1, // @@@@@@@@
+        0,0,1,0,0,1,0,0, // ..@..@..
+        0,1,0,1,1,0,1,0, // .@.@@.@.
+        1,0,1,0,0,1,0,1  // @.@..@.@
+    };
+
+    alien_sprites[2].width = 11;
+    alien_sprites[2].height = 8;
+    alien_sprites[2].data = new uint8_t[88]
+    {
+        0,0,1,0,0,0,0,0,1,0,0, // ..@.....@..
+        0,0,0,1,0,0,0,1,0,0,0, // ...@...@...
+        0,0,1,1,1,1,1,1,1,0,0, // ..@@@@@@@..
+        0,1,1,0,1,1,1,0,1,1,0, // .@@.@@@.@@.
+        1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+        1,0,1,1,1,1,1,1,1,0,1, // @.@@@@@@@.@
+        1,0,1,0,0,0,0,0,1,0,1, // @.@.....@.@
+        0,0,0,1,1,0,1,1,0,0,0  // ...@@.@@...
+    };
+
+    alien_sprites[3].width = 11;
+    alien_sprites[3].height = 8;
+    alien_sprites[3].data = new uint8_t[88]
     {
         0,0,1,0,0,0,0,0,1,0,0, // ..@.....@..
         1,0,0,1,0,0,0,1,0,0,1, // @..@...@..@
@@ -142,35 +182,92 @@ int main()
         0,1,0,0,0,0,0,0,0,1,0  // .@.......@.
     };
 
-    SpriteAnimation* alien_animation = new SpriteAnimation;
+    alien_sprites[4].width = 12;
+    alien_sprites[4].height = 8;
+    alien_sprites[4].data = new uint8_t[96]
+    {
+        0,0,0,0,1,1,1,1,0,0,0,0, // ....@@@@....
+        0,1,1,1,1,1,1,1,1,1,1,0, // .@@@@@@@@@@.
+        1,1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@@
+        1,1,1,0,0,1,1,0,0,1,1,1, // @@@..@@..@@@
+        1,1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@@
+        0,0,0,1,1,0,0,1,1,0,0,0, // ...@@..@@...
+        0,0,1,1,0,1,1,0,1,1,0,0, // ..@@.@@.@@..
+        1,1,0,0,0,0,0,0,0,0,1,1  // @@........@@
+    };
 
-    alien_animation->loop = true;
-    alien_animation->num_frames = 2;
-    alien_animation->frame_duration = 10;
-    alien_animation->time = 0;
 
-    alien_animation->frames = new Sprite * [2];
-    alien_animation->frames[0] = &alien_sprite;
-    alien_animation->frames[1] = &alien_sprite1;
+    alien_sprites[5].width = 12;
+    alien_sprites[5].height = 8;
+    alien_sprites[5].data = new uint8_t[96]
+    {
+        0,0,0,0,1,1,1,1,0,0,0,0, // ....@@@@....
+        0,1,1,1,1,1,1,1,1,1,1,0, // .@@@@@@@@@@.
+        1,1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@@
+        1,1,1,0,0,1,1,0,0,1,1,1, // @@@..@@..@@@
+        1,1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@@
+        0,0,1,1,1,0,0,1,1,1,0,0, // ..@@@..@@@..
+        0,1,1,0,0,1,1,0,0,1,1,0, // .@@..@@..@@.
+        0,0,1,1,0,0,0,0,1,1,0,0  // ..@@....@@..
+    };
 
+    Sprite alien_death_sprite;
+    alien_death_sprite.width = 13;
+    alien_death_sprite.height = 7;
+    alien_death_sprite.data = new uint8_t[91]
+    {
+        0,1,0,0,1,0,0,0,1,0,0,1,0, // .@..@...@..@.
+        0,0,1,0,0,1,0,1,0,0,1,0,0, // ..@..@.@..@..
+        0,0,0,1,0,0,0,0,0,1,0,0,0, // ...@.....@...
+        1,1,0,0,0,0,0,0,0,0,0,1,1, // @@.........@@
+        0,0,0,1,0,0,0,0,0,1,0,0,0, // ...@.....@...
+        0,0,1,0,0,1,0,1,0,0,1,0,0, // ..@..@.@..@..
+        0,1,0,0,1,0,0,0,1,0,0,1,0  // .@..@...@..@.
+    };
 
     Sprite player_sprite;
     player_sprite.width = 11;
     player_sprite.height = 7;
     player_sprite.data = new uint8_t[77]
     {
-        0,0,0,0,0,1,0,0,0,0,0, 
-        0,0,0,0,1,1,1,0,0,0,0,
-        0,0,0,0,1,1,1,0,0,0,0, 
-        0,1,1,1,1,1,1,1,1,1,0, 
-        1,1,1,1,1,1,1,1,1,1,1, 
-        1,1,1,1,1,1,1,1,1,1,1, 
-        1,1,1,1,1,1,1,1,1,1,1, 
+        0,0,0,0,0,1,0,0,0,0,0, // .....@.....
+        0,0,0,0,1,1,1,0,0,0,0, // ....@@@....
+        0,0,0,0,1,1,1,0,0,0,0, // ....@@@....
+        0,1,1,1,1,1,1,1,1,1,0, // .@@@@@@@@@.
+        1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+        1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+        1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
     };
+
+    Sprite bullet_sprite;
+    bullet_sprite.width = 1;
+    bullet_sprite.height = 3;
+    bullet_sprite.data = new uint8_t[3]
+    {
+        1, // @
+        1, // @
+        1  // @
+    };
+
+
+    SpriteAnimation alien_animation[3];
+
+    for (size_t i = 0; i < 3; ++i)
+    {
+        alien_animation[i].loop = true;
+        alien_animation[i].num_frames = 2;
+        alien_animation[i].frame_duration = 10;
+        alien_animation[i].time = 0;
+
+        alien_animation[i].frames = new Sprite * [2];
+        alien_animation[i].frames[0] = &alien_sprites[2 * i];
+        alien_animation[i].frames[1] = &alien_sprites[2 * i + 1];
+    }
 
     Game game;
     game.width = buffer_width;
     game.height = buffer_height;
+    game.num_bullets = 0;
     game.num_aliens = 55;
     game.aliens = new Alien[game.num_aliens];
 
@@ -183,78 +280,153 @@ int main()
     {
         for (size_t xi = 0; xi < 11; ++xi)
         {
-            game.aliens[yi * 11 + xi].x = 16 * xi + 20;
-            game.aliens[yi * 11 + xi].y = 17 * yi + 128;
+            Alien& alien = game.aliens[yi * 11 + xi];
+            alien.type = (5 - yi) / 2 + 1;
+
+            const Sprite& sprite = alien_sprites[2 * (alien.type - 1)];
+
+            alien.x = 16 * xi + 20 + (alien_death_sprite.width - sprite.width) / 2;
+            alien.y = 17 * yi + 128;
         }
     }
 
-    while (!glfwWindowShouldClose(window))
+    uint8_t* death_counters = new uint8_t[game.num_aliens];
+    for (size_t i = 0; i < game.num_aliens; ++i)
     {
-		// Clear the CPU buffer 
+        death_counters[i] = 10;
+    }
+
+    uint32_t clear_color = rgb_to_uint32(0, 128, 0);
+
+    game_running = true;
+
+    int player_move_dir = 0;
+    while (!glfwWindowShouldClose(window) && game_running)
+    {
         buffer_clear(&buffer, clear_color);
 
+        // Draw
         for (size_t ai = 0; ai < game.num_aliens; ++ai)
         {
+            if (!death_counters[ai]) continue;
+
             const Alien& alien = game.aliens[ai];
-            buffer_draw_sprite(&buffer, alien_sprite,
-                alien.x, alien.y, rgb_to_uint32(128, 0, 0));
-        }
-
-        //Draws the player into the CPU buffer
-        buffer_draw_sprite(&buffer, player_sprite, game.player.x, game.player.y, rgb_to_uint32(128, 0, 0));
-
-		//Draws the aliens into the CPU buffer using the current animation frame
-        for (size_t ai = 0;  ai < game.num_aliens; ++ai)
-        {
-            const Alien& alien = game.aliens[ai];
-            size_t current_frame = alien_animation->time / alien_animation->frame_duration;
-            const Sprite& sprite = *alien_animation->frames[current_frame];
-            buffer_draw_sprite(&buffer, sprite, alien.x, alien.y, rgb_to_uint32(128, 0, 0));
-        }
-
-		//Update the animation time
-                                        /*Check if a full animation cycle has completed */
-        ++alien_animation->time;
-        if (alien_animation->time == alien_animation->num_frames * alien_animation->frame_duration)
-        {
-            if (alien_animation->loop) alien_animation->time = 0;
+            if (alien.type == ALIEN_DEAD)
+            {
+                buffer_draw_sprite(&buffer, alien_death_sprite, alien.x, alien.y, rgb_to_uint32(128, 0, 0));
+            }
             else
             {
-                delete alien_animation;
-                alien_animation = nullptr;
+                const SpriteAnimation& animation = alien_animation[alien.type - 1];
+                size_t current_frame = animation.time / animation.frame_duration;
+                const Sprite& sprite = *animation.frames[current_frame];
+                buffer_draw_sprite(&buffer, sprite, alien.x, alien.y, rgb_to_uint32(128, 0, 0));
             }
         }
 
-        // Upload the CPU buffer into the GPU
+        for (size_t bi = 0; bi < game.num_bullets; ++bi)
+        {
+            const Bullet& bullet = game.bullets[bi];
+            const Sprite& sprite = bullet_sprite;
+            buffer_draw_sprite(&buffer, sprite, bullet.x, bullet.y, rgb_to_uint32(128, 0, 0));
+        }
+
+        buffer_draw_sprite(&buffer, player_sprite, game.player.x, game.player.y, rgb_to_uint32(128, 0, 0));
+
+        // Update animations
+        for (size_t i = 0; i < 3; ++i)
+        {
+            ++alien_animation[i].time;
+            if (alien_animation[i].time == alien_animation[i].num_frames * alien_animation[i].frame_duration)
+            {
+                alien_animation[i].time = 0;
+            }
+        }
+
         glTexSubImage2D(
             GL_TEXTURE_2D, 0, 0, 0,
             buffer.width, buffer.height,
             GL_RGBA, GL_UNSIGNED_INT_8_8_8_8,
             buffer.data
         );
-        //Clears the openGL buffer
-        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
-        //Samples the CPU buffer
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		// Swaps the back buffer with the front buffer, and after the swaping the back buffer becomes the canvas and for the next swap
         glfwSwapBuffers(window);
 
-        if (game.player.x + player_sprite.width + player_move_dir >= game.width - 1)
+        // Simulate aliens
+        for (size_t ai = 0; ai < game.num_aliens; ++ai)
         {
-            game.player.x = game.width - player_sprite.width - player_move_dir - 1;
-            player_move_dir *= -1;
+            const Alien& alien = game.aliens[ai];
+            if (alien.type == ALIEN_DEAD && death_counters[ai])
+            {
+                --death_counters[ai];
+            }
         }
-        else if ((int)game.player.x + player_move_dir <= 0)
+
+        // Simulate bullets
+        for (size_t bi = 0; bi < game.num_bullets;)
         {
-            game.player.x = 0;
-            player_move_dir *= -1;
+            game.bullets[bi].y += game.bullets[bi].dir;
+            if (game.bullets[bi].y >= game.height || game.bullets[bi].y < bullet_sprite.height)
+            {
+                game.bullets[bi] = game.bullets[game.num_bullets - 1];
+                --game.num_bullets;
+                continue;
+            }
+
+            // Check hit
+            for (size_t ai = 0; ai < game.num_aliens; ++ai)
+            {
+                const Alien& alien = game.aliens[ai];
+                if (alien.type == ALIEN_DEAD) continue;
+
+                const SpriteAnimation& animation = alien_animation[alien.type - 1];
+                size_t current_frame = animation.time / animation.frame_duration;
+                const Sprite& alien_sprite = *animation.frames[current_frame];
+                bool overlap = sprite_overlap_check(
+                    bullet_sprite, game.bullets[bi].x, game.bullets[bi].y,
+                    alien_sprite, alien.x, alien.y
+                );
+                if (overlap)
+                {
+                    game.aliens[ai].type = ALIEN_DEAD;
+                    // NOTE: Hack to recenter death sprite
+                    game.aliens[ai].x -= (alien_death_sprite.width - alien_sprite.width) / 2;
+                    game.bullets[bi] = game.bullets[game.num_bullets - 1];
+                    --game.num_bullets;
+                    continue;
+                }
+            }
+
+            ++bi;
         }
-        else game.player.x += player_move_dir;
 
+        // Simulate player
+        player_move_dir = 2 * move_dir;
 
-        // Prpces all pending events
+        if (player_move_dir != 0)
+        {
+            if (game.player.x + player_sprite.width + player_move_dir >= game.width)
+            {
+                game.player.x = game.width - player_sprite.width;
+            }
+            else if ((int)game.player.x + player_move_dir <= 0)
+            {
+                game.player.x = 0;
+            }
+            else game.player.x += player_move_dir;
+        }
+
+        // Process events
+        if (fire_pressed && game.num_bullets < GAME_MAX_BULLETS)
+        {
+            game.bullets[game.num_bullets].x = game.player.x + player_sprite.width / 2;
+            game.bullets[game.num_bullets].y = game.player.y + player_sprite.height;
+            game.bullets[game.num_bullets].dir = 2;
+            ++game.num_bullets;
+        }
+        fire_pressed = false;
+
         glfwPollEvents();
     }
 
@@ -263,12 +435,20 @@ int main()
 
     glDeleteVertexArrays(1, &fullscreen_triangle_vao);
 
+    for (size_t i = 0; i < 6; ++i)
+    {
+        delete[] alien_sprites[i].data;
+    }
+
+    delete[] alien_death_sprite.data;
+
+    for (size_t i = 0; i < 3; ++i)
+    {
+        delete[] alien_animation[i].frames;
+    }
     delete[] buffer.data;
-	delete[] alien_sprite.data;
-
-
+    delete[] game.aliens;
+    delete[] death_counters;
 
     return 0;
 }
-
-
--- Space Invaders/Space Invaders.h
+++ Space Invaders/Space Invaders.h
@@ -11,49 +11,10 @@
 #include <cstdint>
 
 
-int player_move_dir = 1;
+bool game_running = false;
+int move_dir = 0;
+bool fire_pressed = 0;
 
-struct Buffer {
-    size_t width, height;
-    uint32_t* data;
-};
-
-struct Sprite {
-    size_t width, height;
-    uint8_t* data;
-};
-
-struct Alien {
-    size_t x, y;
-    uint8_t type;
-};
-
-struct Player {
-    size_t x, y;
-    size_t life;
-};
-
-struct Game {
-    size_t width, height;
-    size_t num_aliens;
-    Alien* aliens;
-    Player player;
-};
-
-struct SpriteAnimation {
-
-    bool loop;
-    size_t num_frames;
-    size_t frame_duration;
-    size_t time;
-    Sprite** frames;
-
-};
-
-
-
-
-// Handling error cases
 #define GL_ERROR_CASE(glerror)\
     case glerror: snprintf(error, sizeof(error), "%s", #glerror)
 
@@ -77,78 +38,179 @@ inline void gl_debug(const char* file, int line) {
 
 #undef GL_ERROR_CASE
 
-
-void error_callback(int error, const char* description)
-{
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-void buffer_clear(Buffer* buffer, uint32_t color)
-{
-    for (size_t i = 0; i < buffer->width * buffer->height; i++) {
-        buffer->data[i] = color;
-    }
-}
-
-void validate_shader(GLuint shader, const char* file = 0)
-{
+void validate_shader(GLuint shader, const char* file = 0) {
     static const unsigned int BUFFER_SIZE = 512;
     char buffer[BUFFER_SIZE];
     GLsizei length = 0;
 
     glGetShaderInfoLog(shader, BUFFER_SIZE, &length, buffer);
 
-    if (length > 0)
-    {
-        printf("Shader %d(%s) compile error: %s\n",
-            shader, (file ? file : ""), buffer);
+    if (length > 0) {
+        printf("Shader %d(%s) compile error: %s\n", shader, (file ? file : ""), buffer);
     }
 }
 
-// Draws a sprite into the CPU buffer, using the provided color for the pixels that are set in the sprite data
-void buffer_draw_sprite(
-    Buffer* buffer, const Sprite& sprite,
-    size_t x, size_t y, uint32_t color
-) {
-    for (size_t xi = 0; xi < sprite.width; ++xi)
-    {
-        for (size_t yi = 0; yi < sprite.height; ++yi)
-        {
-            size_t sy = sprite.height - 1 + y - yi;
-            size_t sx = x + xi;
-            if (sprite.data[yi * sprite.width + xi] &&
-                sy < buffer->height && sx < buffer->width)
-            {
-                buffer->data[sy * buffer->width + sx] = color;
-            }
-        }
-    }
-}
-
-bool validate_program(GLuint program)
-{
+bool validate_program(GLuint program) {
     static const GLsizei BUFFER_SIZE = 512;
     GLchar buffer[BUFFER_SIZE];
     GLsizei length = 0;
 
     glGetProgramInfoLog(program, BUFFER_SIZE, &length, buffer);
 
-    if (length > 0)
-    {
+    if (length > 0) {
         printf("Program %d link error: %s\n", program, buffer);
         return false;
     }
 
     return true;
 }
-// Converts RGB values to a single uint32_t value in the format 0xRRGGBBAA
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    switch (key) {
+    case GLFW_KEY_ESCAPE:
+        if (action == GLFW_PRESS) game_running = false;
+        break;
+    case GLFW_KEY_RIGHT:
+        if (action == GLFW_PRESS) move_dir += 1;
+        else if (action == GLFW_RELEASE) move_dir -= 1;
+        break;
+    case GLFW_KEY_LEFT:
+        if (action == GLFW_PRESS) move_dir -= 1;
+        else if (action == GLFW_RELEASE) move_dir += 1;
+        break;
+    case GLFW_KEY_SPACE:
+        if (action == GLFW_RELEASE) fire_pressed = true;
+        break;
+    default:
+        break;
+    }
+}
+
+struct Buffer
+{
+    size_t width, height;
+    uint32_t* data;
+};
+
+struct Sprite
+{
+    size_t width, height;
+    uint8_t* data;
+};
+
+struct Alien
+{
+    size_t x, y;
+    uint8_t type;
+};
+
+struct Bullet
+{
+    size_t x, y;
+    int dir;
+};
+
+struct Player
+{
+    size_t x, y;
+    size_t life;
+};
+
+#define GAME_MAX_BULLETS 128
+
+struct Game
+{
+    size_t width, height;
+    size_t num_aliens;
+    size_t num_bullets;
+    Alien* aliens;
+    Player player;
+    Bullet bullets[GAME_MAX_BULLETS];
+};
+
+struct SpriteAnimation
+{
+    bool loop;
+    size_t num_frames;
+    size_t frame_duration;
+    size_t time;
+    Sprite** frames;
+};
+
+enum AlienType : uint8_t
+{
+    ALIEN_DEAD = 0,
+    ALIEN_TYPE_A = 1,
+    ALIEN_TYPE_B = 2,
+    ALIEN_TYPE_C = 3
+};
+
+void buffer_clear(Buffer* buffer, uint32_t color)
+{
+    for (size_t i = 0; i < buffer->width * buffer->height; ++i)
+    {
+        buffer->data[i] = color;
+    }
+}
+
+bool sprite_overlap_check(
+    const Sprite& sp_a, size_t x_a, size_t y_a,
+    const Sprite& sp_b, size_t x_b, size_t y_b
+)
+{
+    // NOTE: For simplicity we just check for overlap of the sprite
+    // rectangles. Instead, if the rectangles overlap, we should
+    // further check if any pixel of sprite A overlap with any of
+    // sprite B.
+    if (x_a < x_b + sp_b.width && x_a + sp_a.width > x_b &&
+        y_a < y_b + sp_b.height && y_a + sp_a.height > y_b)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void buffer_draw_sprite(Buffer* buffer, const Sprite& sprite, size_t x, size_t y, uint32_t color)
+{
+    for (size_t xi = 0; xi < sprite.width; ++xi)
+    {
+        for (size_t yi = 0; yi < sprite.height; ++yi)
+        {
+            if (sprite.data[yi * sprite.width + xi] &&
+                (sprite.height - 1 + y - yi) < buffer->height &&
+                (x + xi) < buffer->width)
+            {
+                buffer->data[(sprite.height - 1 + y - yi) * buffer->width + (x + xi)] = color;
+            }
+        }
+    }
+}
+
 uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b)
 {
     return (r << 24) | (g << 16) | (b << 8) | 255;
 }
 
-// Parameters for the shader
-const char* vertex_shader =
+static const char* fragment_shader =
+"\n"
+"#version 330\n"
+"\n"
+"uniform sampler2D buffer;\n"
+"noperspective in vec2 TexCoord;\n"
+"\n"
+"out vec3 outColor;\n"
+"\n"
+"void main(void){\n"
+"    outColor = texture(buffer, TexCoord).rgb;\n"
+"}\n";
+
+static const char* vertex_shader =
 "\n"
 "#version 330\n"
 "\n"
@@ -160,20 +222,4 @@ const char* vertex_shader =
 "    TexCoord.y = (gl_VertexID == 1)? 2.0: 0.0;\n"
 "    \n"
 "    gl_Position = vec4(2.0 * TexCoord - 1.0, 0.0, 1.0);\n"
-"}\n"
-;
-
-
-const char* fragment_shader =
-"\n"
-"#version 330\n"
-"\n"
-"uniform sampler2D buffer;\n"
-"noperspective in vec2 TexCoord;\n"
-"\n"
-"out vec3 outColor;\n"
-"\n"
-"void main(void){\n"
-"    outColor = texture(buffer, TexCoord).rgb;\n"
-"}\n"
-;
+"}\n";
